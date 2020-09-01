4d5c7e2b47a9d7c55a7c36d72299ceb488b43268
Dustin Trinh <dustintrinh98@gmail.com>
2020-09-01 00:09:43 -0400

    Messaging Application

---
+++ Flash-Chat/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RegisterViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBOutlet weak var errorMsg: UILabel!
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    self.errorMsg.isHidden = false
+                    print(e.localizedDescription)
+                    self.errorMsg.text = e.localizedDescription
+                }
+                else{
+                    //Navigate CHatViewController
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                }
+            }
+        }
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        errorMsg.isHidden = true
+        
+    }
+    
+}
---
+++ Flash-Chat/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Dustin Trinh on 2020-08-27.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct Message {
+    let sender: String
+    let body: String
+}
